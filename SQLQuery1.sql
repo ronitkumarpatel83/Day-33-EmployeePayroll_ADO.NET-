f5f10a70fdf30a43136514c2b889e42891523550
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-05-08 16:49:55 +0530

    [Ronit]Added : Data range

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,3 +6,8 @@ create procedure spUpdateEmployee
 @EmployeeID int
 As
 Update employee_payroll_DB set BasicPay=@BasicPay where Name=@Name and @EmployeeID=@EmployeeID;
+
+use payroll_service_DB;
+select * from employee_payroll_DB;
+
+select * from employee_payroll_DB where Startdate between CAST('2010-01-01' as Date) and GETDATE();

e60eef35f3e2633d7edc74d7b88d0cf76817ed74
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-05-08 16:54:40 +0530

    [Ronit]Added : Find SUM,AVG,MIN,MAX,COUNT and group by gender

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,3 +11,10 @@ use payroll_service_DB;
 select * from employee_payroll_DB;
 
 select * from employee_payroll_DB where Startdate between CAST('2010-01-01' as Date) and GETDATE();
+
+select SUM(BasicPay) as Total_Salary from employee_payroll_DB;
+select AVG(BasicPay) as Average_Salary from employee_payroll_DB;
+select MIN(BasicPay) as Minimum_Salary from employee_payroll_DB;
+select MAX(BasicPay) as Maximum_Salary from employee_payroll_DB;
+select COUNT(BasicPay) as NUmber_of_Employee from employee_payroll_DB;
+select SUM(BasicPay)as Total_Group_Of_Female from employee_payroll_DB WHERE Gender = 'F' GROUP BY gender;
